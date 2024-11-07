4ed7705a2830aba5f40819c96067d1c75f58d795
uwantaginting <uwanta889@gmail.com>
2024-11-07 10:43:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$nama = "uwanta";
+$umur = "18";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contoh variabel PHP</title>
+</head>
+<body>
+    <h1>Halo, <?php echo $nama; ?>!</h1>
+    <p> Umur Anda adalah <?php echo $umur; ?> tahun,</P>
+</body>
+</html>
+        
